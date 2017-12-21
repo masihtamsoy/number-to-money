4864ad60a27113d767f54d11b1fbe7d6d428d3ee
masihtamsoy <masihtamsoy@gmail.com>
2017-12-21 13:16:17 +0530

    create company instance and save data

--- models/company.py
+++ models/company.py
@@ -1,19 +1,23 @@
-from django.db import models
-
-from djangotoolbox.fields import ListField
-# Create your models here.
+from mongoengine import Document, EmbeddedDocument, fields# Create your models here.
 from companyData import data
 
 
-class Company(models.Model):
-  prime = models.CharField()
-
+class Company(Document):
+  prime = fields.StringField(verbose_name="prime")
+  number_set = fields.DictField(verbose_name='Number set')
+  bse_code = fields.StringField(verbose_name='BSE code')
+  short_name = fields.StringField(verbose_name='Short name')
+  nse_code = fields.StringField(verbose_name='NSE code')
+  companyrating_set = fields.ListField(verbose_name='Rating')
+  annualreport_set = fields.ListField(verbose_name='Annual report')
+  announcement_set = fields.ListField(verbose_name='Announcement')
+  warehouse_set = fields.DictField(verbose_name='Warehouse set')
+  name = fields.StringField(verbose_name='name')
+  id = fields.IntField(verbose_name='id', primary_key=True)
 
 def populateCompany():
-  company = Company(
-    prime='Masih'
-  )
+  company = Company(**data)
   company.save()
 
 
-# from models.company import Company, populateCompany
+# from models.company import *
--- numbers_to_money/settings.py
+++ numbers_to_money/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from mongoengine import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -57,15 +58,7 @@ WSGI_APPLICATION = 'numbers_to_money.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default' : {
-      'ENGINE' : 'django_mongodb_engine',
-      'NAME' : 'ntmoney'
-   }
-}
-
-
+connect('ntmoney')
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
